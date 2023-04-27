6f779dc0dd1721bbf62afe60ec306fa491ccbab0
Esra <esraadiller2@gmail.com>
2023-04-27 15:21:19 +0200

    payment

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,18 +11,15 @@ before(() => {
             cy.url().should('include', 'index.html')
             cy.get('#signin_button').click()
         cy.get('#login_form').should('be.visible')
-        cy.get('#user_login').type('invalid username')
-        cy.get('#user_password').type('invalid password')
-        cy.contains('Sign in').click()
+        
+        cy.login("invalid username", "invalid password")
         cy.get('.alert-error').should('be.visible').and('contain', 'Login and/or password are wrong')
         cy.fixture('user').then(user => {
             const username = user.id
             const password = user.pwd
     
-            cy.get('#user_login').type(username)
-            cy.get('#user_password').type(password)
-            cy.get('#user_remember_me').click()
-            cy.contains('Sign in').click()
+           
+            cy.login (username, password)
         })
         cy.get('ul.nav-tabs').should('be.visible')
         cy.contains('username').click()
